a2e3a590ec1f75790e1d50d419f0e8db9ccf23c5
らいか <raika4789@gmail.com>
2022-07-01 00:50:45 +0900

    norm

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtakano   <rtakano@student.42.fr    >      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/19 19:48:07 by rtakano           #+#    #+#             */
+/*   Updated: 2022/07/01 00:42:03 by rtakano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
@@ -5,25 +17,25 @@ char	*get_next_line(int fd)
 	static int	read_res;
 	char		c;
 	int			i;
-	char	*res;
+	char		*res;
 
 	read_res = 1;
 	i = 0;
-	if(read_res == 0)
+	if (read_res == 0)
 		return (NULL);
 	res = malloc(BUFFER_SIZE + 1);
 	while (i < BUFFER_SIZE)
 	{
 		read_res = read(fd, &c, 1);
-		if(read_res == 0)
+		if (read_res == 0)
 			return (res);
-		if(c == '\0')
+		if (c == '\0')
 			return (res);
 		res[i] = c;
-		if(c == '\n')
-			return(res);
+		if (c == '\n')
+			return (res);
 		i++;
 	}
 	res[BUFFER_SIZE] = '\0';
-	return(res);
+	return (res);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtakano   <rtakano@student.42.fr    >      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/19 19:48:14 by rtakano           #+#    #+#             */
+/*   Updated: 2022/07/01 00:24:50 by rtakano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include	<stdlib.h>
-#include	<unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 
